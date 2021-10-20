d40540ed3b229e42791220a86875c6a799f43861
Anthony Bousser <anthony.bousser@epita.fr>
2021-10-20 15:10:28 +0200

    Update exercice-5.js

---
+++ day-1/exercice-5.js
@@ -0,0 +1,9 @@
+export const my_array_alpha = (str) => {
+    var charArray = [];
+    var i = 0;
+    while (str[i] != undefined) {
+        charArray[i] = str[i];
+        i++;
+    }
+    return charArray;
+}
