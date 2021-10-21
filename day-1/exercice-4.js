61702cd2689f325b392ae0c3df9b3a8aa23d124b
Anthony Bousser <anthony.bousser@epita.fr>
2021-10-21 11:45:18 +0200

    modif

--- day-1/exercice-4.js
+++ day-1/exercice-4.js
@@ -2,7 +2,7 @@ export const my_size_alpha = (str) => {
     if (typeof(str) != "string")
         return 0;
     var i = 0;
-    while (str[i] != undefined)
+    while (typeof(str[i]) == 'string')
         i++;
     return i;
 }
